4cd3db0a8362820fc5cb6e1da17dd5dd408815f8
Lucas Rodrigues <Lucasrodrigues4@live.com>
2024-09-14 13:18:18 -0300

    calculadora

--- calculadora.js
+++ calculadora.js
@@ -1,19 +1,19 @@
 function calcular(tipo, valor) {
   if (tipo === "acao") {
-
-        if (valor === "c") {
+    if (valor === "c") {
       // limpar visor(id resultado)
       document.getElementById("resultado").value = "";
-        }
+    }
 
-        if(valor==="+" || valor==="-"|| valor==="*"|| valor ==="/"||valor==="."){
-            document.getElementById("resultado").value+=valor
-         }
-        if(valor==="="){
-         let valor_campo =eval(document.getElementById('resultado').value)
-            //fazer de string virar numero (eval)
-            document.getElementById("resultado").value=valor_campo
-        }
+    if (valor === "+" || valor === "-" || valor === "*" || valor === "/" || valor === "."
+    ) {
+      document.getElementById("resultado").value += valor;
+    }
+    if (valor === "=") {
+      let valor_campo = eval(document.getElementById("resultado").value);
+      //fazer de string virar numero (eval)
+      document.getElementById("resultado").value = valor_campo;
+    }
   } else if (tipo === "valor") {
     // let valor_campo=document.getElementById("resultado").value
     //para deixar menor o codigo colocamos o +=
