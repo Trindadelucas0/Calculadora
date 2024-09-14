68282ecee85e965c25ca6781645094560ed2f5f4
Lucas Rodrigues <Lucasrodrigues4@live.com>
2024-09-14 13:16:14 -0300

    Initial commit

---
+++ calculadora.js
@@ -0,0 +1,22 @@
+function calcular(tipo, valor) {
+  if (tipo === "acao") {
+
+        if (valor === "c") {
+      // limpar visor(id resultado)
+      document.getElementById("resultado").value = "";
+        }
+
+        if(valor==="+" || valor==="-"|| valor==="*"|| valor ==="/"||valor==="."){
+            document.getElementById("resultado").value+=valor
+         }
+        if(valor==="="){
+         let valor_campo =eval(document.getElementById('resultado').value)
+            //fazer de string virar numero (eval)
+            document.getElementById("resultado").value=valor_campo
+        }
+  } else if (tipo === "valor") {
+    // let valor_campo=document.getElementById("resultado").value
+    //para deixar menor o codigo colocamos o +=
+    document.getElementById("resultado").value += valor;
+  }
+}
