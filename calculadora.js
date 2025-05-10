8ea34561d6480df63fae19ed6e60f218a550cc07
Lucas Rodrigues <Lucasrodrigues4@live.com>
2025-05-10 14:02:23 -0300

    ok

--- calculadora.js
+++ calculadora.js
@@ -1,22 +1,70 @@
-function calcular(tipo, valor) {
-  if (tipo === "acao") {
-    if (valor === "c") {
-      // limpar visor(id resultado)
-      document.getElementById("resultado").value = "";
-    }
+const main = document.querySelector("main")
+const root = document.querySelector(":root")
+const input = document.getElementById("input")
+const resultInput = document.getElementById("result")
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
-    if (valor === "+" || valor === "-" || valor === "*" || valor === "/" || valor === "."
-    ) {
-      document.getElementById("resultado").value += valor;
-    }
-    if (valor === "=") {
-      let valor_campo = eval(document.getElementById("resultado").value);
-      //fazer de string virar numero (eval)
-      document.getElementById("resultado").value = valor_campo;
-    }
-  } else if (tipo === "valor") {
-    // let valor_campo=document.getElementById("resultado").value
-    //para deixar menor o codigo colocamos o +=
-    document.getElementById("resultado").value += valor;
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", function () {
+    const value = charKeyBtn.dataset.value
+    input.value += value
+  })
+})
+
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = ""
+  input.focus()
+})
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault()
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key
+    return
   }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1)
+  }
+  if (ev.key === "Enter") {
+    calculate()
+  }
+})
+
+document.getElementById("equal").addEventListener("click", calculate)
+
+function calculate() {
+  resultInput.value = "ERROR"
+  resultInput.classList.add("error")
+  const result = eval(input.value)
+  resultInput.value = result
+  resultInput.classList.remove("error")
 }
+//botão ficar verde
+document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
+  const button = ev.currentTarget
+  if (button.innerText === "Copy") {
+    button.innerText = "Copied!"
+    button.classList.add("success")
+    navigator.clipboard.writeText(resultInput.value)
+  } else {
+    button.innerText = "Copy"
+    button.classList.remove("success")
+  }
+})
+
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9")
+    root.style.setProperty("--border-color", "#aaa")
+    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--primary-color", "#26834a")
+    main.dataset.theme = "light"
+  } else {
+    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--font-color", "#f1f5f9")
+    root.style.setProperty("--primary-color", "#4dff91")
+    main.dataset.theme = "dark"
+  }
+})
